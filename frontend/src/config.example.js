52a988c2113fb1f6c75da139fca32fc79538308c
202201180 <202201180@daiict.ac.in>
2024-11-15 00:51:53 +0530

    Delete frontend directory

--- frontend/src/config.example.js
+++
@@ -1,5 +0,0 @@
-const config = {
-  BACKEND_API: YOUR_BACKEND_API,
-};
-
-export default config;
--- frontend/src/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-  BACKEND_API: "http://localhost:8000",
-};
-
-export default config;
