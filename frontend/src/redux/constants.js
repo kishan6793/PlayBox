fd873263487affd014341f547251913dc68bdb89
202201180 <202201180@daiict.ac.in>
2024-11-21 17:01:52 +0530

    Update constants.js

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -2,13 +2,13 @@
 export const BASE_URL = "";
 
 // URL for user-related API requests (e.g., user authentication, profile management)
-export const USERS_URL = "https://playbox-5v09.onrender.com";
+export const USERS_URL = "https://playbox-5v09.onrender.com/api/v1/users";
 
 // URL for genre-related API requests (e.g., fetching genres, adding new genres)
-export const GENRE_URL = "https://playbox-5v09.onrender.com";
+export const GENRE_URL = "https://playbox-5v09.onrender.com/api/v1/genre";
 
 // URL for movie-related API requests (e.g., CRUD operations for movies)
-export const MOVIE_URL = "https://playbox-5v09.onrender.com";
+export const MOVIE_URL = "https://playbox-5v09.onrender.com/api/v1/movies";
 // Alternate version of the MOVIE_URL (commented out, can be used for relative paths)
 // export const MOVIE_URL = "/api/v1/movies";
 
